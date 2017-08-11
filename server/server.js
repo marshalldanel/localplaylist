3eaf7dc1e09a01b648dc46c444fe5759fe949d5d
Marshall Danel <marshalldanel@gmail.com>
2017-08-11 16:44:14 -0700

    Update server.js and server package

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const PORT = 3001;
+
+const server = express()
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  
