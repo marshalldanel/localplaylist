e5d39c8eaff9a6f43da72f7e4af5d0ff9b535042
Denny Hollick <dennyhollick@gmail.com>
2017-08-15 10:56:53 -0700

    cards now include links to tickets, updated styling

--- app/src/ConcertCard.jsx
+++ app/src/ConcertCard.jsx
@@ -17,25 +17,27 @@ class ConcertCard extends Component {
                 <div className="media">
                   <div className="media-left">
                     <figure className="image is-96x96">
-                      <img src={concert.image.medium.url} alt="Image"/>
+                       <img src={concert.image.medium.url} alt="Image"/> 
                     </figure>
                   </div>
                   <div className="media-content">
-                    <p className="title is-4">{concert.title}</p>
+                    <p className="title is-5">{concert.title}</p>
                     <p className="subtitle is-6">
-                      <a href={concert.venue_url}>{concert.venue_name}</a>
+                      <a href={concert.venue_url}>{concert.venue_name.replace(', The', '')}</a>
                       <br/>
+                      <small>
                       {concert.start_time}
                       <br/>
                       {concert.venue_address}, {concert.city_name}
+                      </small>
                     </p>
 
                   </div>
                 </div>
               </div>
               <footer className="card-footer">
-                <a className="card-footer-item">Venue Info</a>
-                <a className="card-footer-item is-danger">Get Tickets</a>
+                <a href={concert.venue_url} className="card-footer-item">Venue Info</a>
+                <a href={concert.url} className="card-footer-item is-danger">Get Tickets</a>
               </footer>
             </div>
           </div>
