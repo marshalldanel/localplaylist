dc9b16cb162c6860a047d37649b72806394b1ee1
Denny Hollick <dennyhollick@gmail.com>
2017-08-14 18:09:44 -0700

    Created concert card component

---
+++ app/src/ConcertCard.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ConcertCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+export default ConcertCard;
