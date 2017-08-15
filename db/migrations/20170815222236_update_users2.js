8e98ec15948239a9f9180b6c47d6690b12832ff4
Marshall Danel <marshalldanel@gmail.com>
2017-08-15 15:40:06 -0700

    Update users, remove notNull clause, added user_cookie column

---
+++ db/migrations/20170815222236_update_users2.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('users', function(table){
+      table.string('first_name');
+      table.string('last_name');
+      table.string('email').unique();
+      table.string('password_digest');
+      table.string('user_cookie').unique();
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  table.dropColumn('first_name', 'last_name', 'email', 'password_digest', 'user_cookie');
+};
