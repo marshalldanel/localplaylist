ecc55d2d71a0c5de3d94cd5b91a5343382d1222c
Denny Hollick <dennyhollick@gmail.com>
2017-08-16 13:41:15 -0700

    Added playlist elements

--- app/src/PlaylistSong.jsx
+++ app/src/PlaylistSong.jsx
@@ -8,19 +8,20 @@ class PlaylistSong extends Component {
   render() {
 
     const song = this.props.song;
+    const index = this.props.index + 1;
 
     return (
-     <div className="container">
+
       <div className="box is-marginless is-radiusless">
         <div className="columns">
           <div className="column is-1 has-text-centered">
-            # 1
+            {index}
           </div>
-          <div className="column is-3">
-            Artist Name
+          <div className="column is-4">
+            {song.artist}
           </div>
-          <div className="column is-5">
-            Song Title
+          <div className="column is-4">
+            {song.title}
           </div>
           <div className="column is-3 has-text-right">
             <span className="icon">
@@ -35,55 +36,6 @@ class PlaylistSong extends Component {
           </div>
         </div> 
       </div>
-      <div className="box is-marginless is-radiusless">
-        <div className="columns">
-          <div className="column is-1 has-text-centered">
-            # 1
-          </div>
-          <div className="column is-3">
-            Artist Name
-          </div>
-          <div className="column is-5">
-            Song Title
-          </div>
-          <div className="column is-3 has-text-right">
-            <span className="icon">
-              <i className="fa fa-play-circle"></i>
-            </span>  
-            <span className="icon">
-              <i className="fa fa-stop-circle"></i>
-            </span> 
-            <span className="icon">
-              <i className="fa fa-trash"></i>
-            </span> 
-          </div>
-        </div> 
-      </div>
-      <div className="box is-marginless is-radiusless">
-        <div className="columns">
-          <div className="column is-1 has-text-centered">
-            # 1
-          </div>
-          <div className="column is-3">
-            Artist Name
-          </div>
-          <div className="column is-5">
-            Song Title
-          </div>
-          <div className="column is-3 has-text-right">
-            <span className="icon">
-              <i className="fa fa-play-circle"></i>
-            </span>  
-            <span className="icon">
-              <i className="fa fa-stop-circle"></i>
-            </span> 
-            <span className="icon">
-              <i className="fa fa-trash"></i>
-            </span> 
-          </div>
-        </div> 
-      </div>
-    </div>
     );
   }
 }
