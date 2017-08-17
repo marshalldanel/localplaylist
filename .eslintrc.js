88556bb1e151c4f28e454483414a8f371b4d5d1d
Marshall Danel <marshalldanel@gmail.com>
2017-08-17 13:28:44 -0700

    Added 2 new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     "rules": {
         "react/prop-types": 0,
         "import/extensions": 0,
-        "react/prefer-stateless-function": 0
+        "react/prefer-stateless-function": 0,
+        "arrow-body-style": 0
     },
 
 };
