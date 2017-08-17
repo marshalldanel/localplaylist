49838ae802a19e3b97eacc49a463be2fcb6ed67b
Marshall Danel <marshalldanel@gmail.com>
2017-08-17 16:40:56 -0700

    Actual bug fix with inserting users into DB

--- server/routes/trip.js
+++ server/routes/trip.js
@@ -2,25 +2,23 @@ module.exports = knex => ({
 
   saveTrip: (tripData) => {
     return knex.transaction(() => {
-      return knex('users').select('id').where('user_cookie', tripData.cookie)
-        .returning('id')
-        .then((id) => {
-          if (!id.length) {
+      return knex('users').select('id').where('user_cookie', 'SkQnAKmdZ')
+        .then((ids) => {
+          if (!ids.length) {
             return knex('users')
               .insert({
                 user_cookie: tripData.cookie,
               }).returning('id');
           }
-          return knex('users').select('id').where('id', id[0].id)
-            .returning('id')
-            .then((user_id) => {
-              return knex('trips')
-                .insert({
-                  name: tripData.name,
-                  user_id: id[0].id,
-                })
-                .returning('id');
-            });
+          return ids;
+        })
+        .then((new_ids) => {
+          return knex('trips')
+            .insert({
+              name: tripData.name,
+              user_id: new_ids[0].id,
+            })
+            .returning('id');
         });
     });
   },
