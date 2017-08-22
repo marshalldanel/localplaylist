2444784ca2d436e88104414b6042b14d30d46ee7
Denny Hollick <dennyhollick@gmail.com>
2017-08-22 11:13:06 -0700

    Changed addlocation style, created remove location

--- app/src/LocationsList.jsx
+++ app/src/LocationsList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { addLocationField } from './actions.js';
+import { addLocationField, removeLocationField } from './actions.js';
 import LocationField from './LocationField.jsx';
 
 
@@ -31,7 +31,16 @@ class LocationsList extends Component {
         <div className="columns">
           <div className="column is-5" />
           <div className="column is-2">
-            <button className="button is-primary is-outlined has-text-centered" onClick={() => { addLocationField(); }}>Add Another Location </button>
+            <button className="button is-primary is-outlined has-text-centered" onClick={() => { addLocationField(); }}>
+              <div className="icon">
+                <i className="fa fa-plus" />
+              </div>
+            </button>
+            <button className="button is-primary is-outlined has-text-centered" onClick={() => { removeLocationField(); }}>
+              <div className="icon">
+                <i className="fa fa-minus" />
+              </div>
+            </button>
           </div>
         </div>
       </div>
